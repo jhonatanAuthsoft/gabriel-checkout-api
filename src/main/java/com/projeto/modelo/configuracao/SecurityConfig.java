2aafe23969b5a5830ef47ed8b216e607ac1fa9c5
JhonatanKramer <jckramer@universo.univates.br>
2025-06-27 17:39:57 -0300

    swagger

--- src/main/java/com/projeto/modelo/configuracao/SecurityConfig.java
+++ src/main/java/com/projeto/modelo/configuracao/SecurityConfig.java
@@ -36,7 +36,9 @@ public class SecurityConfig {
                                 "/usuario/cadastrar",
                                 "/usuario/esqueceu-senha",
                                 "/usuario/validar-troca-senha",
-                                "/swagger-ui/*"
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**"
                               ).permitAll() // Permitir endpoints públicos
                         .anyRequest().authenticated() // Exigir autenticação para os demais endpoints
                 )
